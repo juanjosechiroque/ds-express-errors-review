1bb6bbaac189f0970750116f4df44fc500c257d3
Juan José Chiroque <juanjosechiroque@gmail.com>
2026-08-14 13:01:49 -0500

    refactor: use library in notfound middleware

--- src/middleware/notFoundMiddleware.ts
+++ src/middleware/notFoundMiddleware.ts
@@ -1,6 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
-import { NotFoundError } from "../errors.js";
+import { Errors } from "ds-express-errors";
 
 export function notFound(_req: Request, _res: Response, next: NextFunction) {
-    next(NotFoundError("Route not found"));
+    next(Errors.NotFound("Route not found"));
 }
